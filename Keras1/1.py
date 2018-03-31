6b01468b53fb693aded98236199378554143c740
qw <2768918076@qq.com>
2018-03-31 16:32:55 +0800

    The Second DeepLearningModel

--- Keras1/1.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-# classifier = tf.estimator.LinearClassifier();
-# classifier.train(input_fn = train_input_fn,step = 2000)
-cn = tf.constant("tensorflow")
-sess = tf.Session();
-sess.run(cn)
-
